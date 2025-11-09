d739fa3c7ec80622e34124f997e3b390a4fc3e4b
clayton <claytonhouser10@gmail.com>
2025-11-08 23:54:26 -0600

    adding random plant generator

--- Backend/plantnet/views.py
+++ Backend/plantnet/views.py
@@ -13,73 +13,57 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 plantnet_api = "https://my-api.plantnet.org/v2/"
     
+dificulties = {
+    'easy': 1,
+    'mid': 2,
+    'hard': 3
+}
 
 @csrf_exempt
 @require_POST
 def get_random_plant(request):
-    #define parameters for the request
-    #Get users current latitudre and longitude
-    usersLat=None
-    usersLon=None
-    boxSize=None
+    try:
+        body = request.body
+
+        data = json.loads(body)
+
+        difficulty = data.get('difficulty', None)
+
+        if not difficulty:
+            return JsonResponse({"error": "Missing 'scientific-name' field"}, status=400)
+        
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "invalid JSON"}, status=400)
+    
+    level_id=dificulties.get(difficulty)
+
+    if not level_id:
+        return JsonResponse({'error': "Invalid level"},status=400)
+    
+    #get random plant inc ategory
 
     try:
-        data = json.loads(request.body.decode('utf-8'))
+        plants_in_level = Plant.objects.filter(dificulty=level_id)
 
-        #get args
-        usersLat=data.get('lat')
-        usersLon=data.get('lon')
-        boxSize=data.get('size')
-
-        if not (usersLat is not None and usersLon is not None and boxSize is not None):
-            #arguemtns not correct
-            return JsonResponse({'status': 'error', 'message': 'incorrect fields provided'}, status=400)
+        if not plants_in_level.exists():
+            return JsonResponse({"error": f"No plants for dificulty"}, status=404)
+        random_plant = plants_in_level.order_by('?').first()#find random plant
     except Exception as e:
-        return JsonResponse({
-            'status': 'error',
-            'message': f'Error translating inputed json: {str(e)}'
-        })
-
+        return JsonResponse({"error": "Internal database error"}, status=500)
     
-    #define the arguments for our api request
-    params = {
-        'topLeftLat': usersLat+boxSize,
-        'topLeftLon': usersLon-boxSize,
-        'bottomRightLat': usersLat-boxSize,
-        'bottomRightLon': usersLon+boxSize,
-        'api-key': settings.PLANTNET_API_KEY,
-        'lang': 'en'
+    result = {
+        'scientific-name': random_plant.scientific_name,
+        'description': random_plant.description,
+        'common-name': random_plant.common_name,
+        'dificulty': random_plant.dificulty,
+        'image-url': random_plant.image.url
     }
 
-    endpoint=f"{plantnet_api}/prediction/geo/species"
+    print(result)
 
-    #make the request
-    try:
-        response = requests.get(endpoint, params=params)
-
-        #check for success
-        if response.status_code == 200:
-            #success
-            data = response.json()
-            print(data['results'])
-            return JsonResponse({'status': 'success', 'results': data['results']}, status=response.status_code)
-        else:
-            #failed
-            print(f"Error: {response.status_code}")
-            print(response.text)
-            return JsonResponse({
-                'status': 'error',
-                'message': 'Plantnet API returned error',
-                'api_status': response.status_code,
-                'api_details': response.text
-            }, status=response.status_code)
+    return JsonResponse(result, status=200, safe=False)
     
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return JsonResponse({
-            'status': 'error',
-            'message': f'Error using Plantnet API: {str(e)}'
-        }, status=503)
+
     
 def getimageresults(request):
     organ = request.POST.get('organs', 'auto')#tell plantnet to detect what we are sending
