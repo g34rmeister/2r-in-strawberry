98bb27db15e39539c79951b63fd9c7e79d0d36a9
g34rmeister <gamerkg123@gmail.com>
2025-11-09 03:43:49 -0600

    fixing merge conflicts 6 7

--- Frontend/logoffproject/src/App.jsx
+++ Frontend/logoffproject/src/App.jsx
@@ -2,12 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route, Link } from 'react-router-dom';
 import Login from "../src/pages/login"
-<<<<<<< HEAD
 import StartChallenge from './pages/start-challenge';
 import LeaderboardCardExample from './pages/leaderboardCardExample';
-=======
 import Challenges from './pages/challenges';
->>>>>>> challengeView
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,13 +13,10 @@ function App() {
     <>
       <Routes>
         <Route path="/login" element={<Login/>} />
-<<<<<<< HEAD
         <Route path="/start-challenge" element={<StartChallenge/>} />
         <Route path="/" element={<Login />} />
         <Route path="/leaderboardcardexample" element={<LeaderboardCardExample/>} />
-
         <Route path="/challenges" element={<Challenges/>} />
->>>>>>> challengeView
       </Routes>
     </>
   )
