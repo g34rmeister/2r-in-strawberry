ababbfed24f905268a81295c71b02fb360668342
g34rmeister <gamerkg123@gmail.com>
2025-11-09 00:45:20 -0600

    formatted leaderboard

--- Frontend/logoffproject/src/pages/leaderboardCardExample.jsx
+++ Frontend/logoffproject/src/pages/leaderboardCardExample.jsx
@@ -3,14 +3,19 @@ import api from '../components/api'
 import { useState } from 'react';
 import LeaderboardCard from '../components/leaderboardCard.jsx'
 import '../components/leaderboardCard.css'
+import Navbar from '../components/navbar';
 
 function LeaderboardCardExample() {
+
     return (
+        <>
         <div>
-            <h1>HelloWorld</h1>
+            <h1></h1>
             <LeaderboardCard/>
 
         </div>
+        <Navbar />
+        </>
     );
 }
 
