7e4307084529bc0061c7b3215644075f2840472b
Maby <maby200@gmail.com>
2025-11-09 09:56:25 -0600

    chore editing challenges propperties

--- Frontend/logoffproject/src/pages/challenges.jsx
+++ Frontend/logoffproject/src/pages/challenges.jsx
@@ -1,4 +1,5 @@
 import Card from "../components/card";
+import api from "../components/api";
 
 const Challenges = ({imgUrl,title,text}) => {
     imgUrl = !imgUrl ? "https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/476148433_1167612378274157_7142023286726290425_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Obsy9c9Xd7cQ7kNvwEX2H4a&_nc_oc=Adm67Qd9u95Ncyqny24wkL9bOs-y-41z6CiED6j7JdwgWzdIR3QEqSqpbWcDLU-UtIBjnpyJNVWx5KOTJUdJ4y73&_nc_zt=23&_nc_ht=scontent-dfw5-2.xx&_nc_gid=tHgObA4Dgj61ge3Cq5UlQQ&oh=00_AfizVNfwafVE8j5_OFtbQXZwP1-tCfCIbfRL8AR0sdZD_w&oe=6915BBE9": imgUrl;
@@ -10,9 +11,7 @@ const Challenges = ({imgUrl,title,text}) => {
             <Card
              imgUrl={imgUrl} 
              name = "Southern Live Oak"
-             rarity = "★★" 
-             quality = "10" 
-             dateCollected = "11-9-2025"
+             rarity = "★★"
              title={title} 
              text={text} 
              />
